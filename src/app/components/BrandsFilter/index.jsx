ef0bfe8ea94d4c2e1346c96e14cf05559adc6226
ahmetaydar <a.aydar2014@gmail.com>
2023-12-08 11:04:55 +0300

    fix check icon

--- src/app/components/BrandsFilter/index.jsx
+++ src/app/components/BrandsFilter/index.jsx
@@ -1,74 +1,79 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import SquareCheckedIcon from "../Svgs/SquareCheckedIcon";
-import SquareIcon from "../Svgs/SquareIcon";
 
-const BrandsFilter = ({ setSelectedBrand, products }) => {
-  const [uniqueBrands, setUniqueBrands] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+const BrandsFilter = ({ setSelectedBrand, products, selectedBrand }) => {
+    const [uniqueBrands, setUniqueBrands] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    const getUniqueBrands = () => {
-      const brands = new Set(products.map((product) => product.brand));
-      return ["All", ...brands];
+    useEffect(() => {
+        const getUniqueBrands = () => {
+            const brands = new Set(products.map((product) => product.brand));
+            return ["All", ...brands];
+        };
+
+        setUniqueBrands(getUniqueBrands());
+    }, [products]);
+
+    const handleBrandChange = (brand) => {
+        setSelectedBrand(brand);
     };
 
-    setUniqueBrands(getUniqueBrands());
-  }, [products]);
+    const filteredBrands = uniqueBrands.filter((brand) =>
+        brand.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-  const handleBrandChange = (brand) => {
-    setSelectedBrand(brand);
-  };
+    const getBrandProductCount = (brand) => {
+        if (brand === "All") {
+            return products.length;
+        } else {
+            return products.filter((product) => product.brand === brand).length;
+        }
+    };
 
-  const filteredBrands = uniqueBrands.filter((brand) =>
-    brand.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const getBrandProductCount = (brand) => {
-    if (brand === "All") {
-      return products.length;
-    } else {
-      return products.filter((product) => product.brand === brand).length;
-    }
-  };
-
-  return (
-    <div className="pl-[104px] pt-5">
-      <h1 className="text-greyscale500 text-[13px] font-semibold mb-3">
-        Brands
-      </h1>
-      <div className="w-[296px] h-[244px] container bg-white rounded-sm border-none shadow-sm p-6 hover:border-black300  ">
-        <input
-          type="text"
-          placeholder="Search Brand"
-          className="w-[248px] h-12 ps-3 border-2 rounded-sm outline-none border-black100"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <div className="mt-[17px] max-h-[120px] overflow-auto ">
-          <ul>
-            {filteredBrands.map((brand) => (
-              <li
-                key={brand}
-                className="flex items-center gap-2 "
-                onClick={() => handleBrandChange(brand)}
-              >
-                <span className="hover:cursor-pointer">
-                  <SquareCheckedIcon />
-                </span>
-                <span className="text-black600 font-normal text-sm tracking-wide">
-                  {brand}
-                </span>
-                <span className="text-black300 font-normal tracking-wide ">
-                  ({getBrandProductCount(brand)})
-                </span>
-              </li>
-            ))}
-          </ul>
+    return (
+        <div className="pl-[104px] pt-5">
+            <h1 className="text-greyscale500 text-[13px] font-semibold mb-3">
+                Brands
+            </h1>
+            <div className="w-[296px] h-[244px] container bg-white rounded-sm border-none shadow-sm p-6 hover:border-black300  ">
+                <input
+                    type="text"
+                    placeholder="Search Brand"
+                    className="w-[248px] h-12 ps-3 border-2 rounded-sm outline-none border-black100"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <div className="mt-[17px] max-h-[120px] overflow-auto ">
+                    <ul>
+                        {filteredBrands.map((brand) => (
+                            <li
+                                key={brand}
+                                className="flex items-center gap-2 "
+                                onClick={() => handleBrandChange(brand)}>
+                                <span className="hover:cursor-pointer">
+                                    <img
+                                        src={
+                                            selectedBrand === brand
+                                                ? "/images/squarecheckedicon.svg"
+                                                : "/images/squareicon.svg"
+                                        }
+                                        alt="square"
+                                    />
+                                </span>
+                                <span className="hover:cursor-pointer w-[22px] h-[22px] "></span>
+                                <span className="text-black600 font-normal text-sm tracking-wide">
+                                    {brand}
+                                </span>
+                                <span className="text-black300 font-normal tracking-wide ">
+                                    ({getBrandProductCount(brand)})
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BrandsFilter;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,161 +12,163 @@ import BasketCard from "./components/BasketCard";
 import Navbar from "./components/Navbar";
 
 const Page = () => {
-  const [products, setProducts] = React.useState([]);
-  const [activeCategory, setActiveCategory] = React.useState("mugs");
-  const [onSort, setOnSort] = React.useState("priceLowToHigh");
-  const [selectedBrand, setSelectedBrand] = React.useState("All");
-  const [selectedTags, setSelectedTags] = React.useState("All");
+    const [products, setProducts] = React.useState([]);
+    const [activeCategory, setActiveCategory] = React.useState("mugs");
+    const [onSort, setOnSort] = React.useState("priceLowToHigh");
+    const [selectedBrand, setSelectedBrand] = React.useState("All");
+    const [selectedTags, setSelectedTags] = React.useState("All");
 
-  //pagination start
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const productsPerPage = 16;
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-  const startIndex = (currentPage - 1) * productsPerPage;
-  const endIndex = startIndex + productsPerPage;
-  const displayedProducts = products.slice(startIndex, endIndex);
-  //pagination end
+    //pagination start
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const productsPerPage = 16;
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+    const startIndex = (currentPage - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+    const displayedProducts = products.slice(startIndex, endIndex);
+    //pagination end
 
-  const fetchProducts = async () => {
-    try {
-      const { data } = await axios.get(`/api/products`);
-      let filteredProducts = data.data.filter(
-        (product) => product.category === activeCategory
-      );
+    const fetchProducts = async () => {
+        try {
+            const { data } = await axios.get(`/api/products`);
+            let filteredProducts = data.data.filter(
+                (product) => product.category === activeCategory
+            );
 
-      if (selectedTags !== "All") {
-        filteredProducts = filteredProducts.filter(
-          (product) => product.tags === selectedTags
-        );
-      }
+            if (selectedTags !== "All") {
+                filteredProducts = filteredProducts.filter(
+                    (product) => product.tags === selectedTags
+                );
+            }
 
-      if (selectedBrand !== "All") {
-        filteredProducts = filteredProducts.filter(
-          (product) => product.brand === selectedBrand
-        );
-      }
+            if (selectedBrand !== "All") {
+                filteredProducts = filteredProducts.filter(
+                    (product) => product.brand === selectedBrand
+                );
+            }
 
-      switch (onSort) {
-        case "priceLowToHigh":
-          filteredProducts = filteredProducts.sort((a, b) => a.price - b.price);
-          break;
-        case "priceHighToLow":
-          filteredProducts = filteredProducts.sort((a, b) => b.price - a.price);
-          break;
-        case "dateNewToOld":
-          filteredProducts = filteredProducts.sort(
-            (a, b) => new Date(b.date) - new Date(a.date)
-          );
-          break;
-        case "dateOldToNew":
-          filteredProducts = filteredProducts.sort(
-            (a, b) => new Date(a.date) - new Date(b.date)
-          );
-          break;
-        default:
-          break;
-      }
+            switch (onSort) {
+                case "priceLowToHigh":
+                    filteredProducts = filteredProducts.sort(
+                        (a, b) => a.price - b.price
+                    );
+                    break;
+                case "priceHighToLow":
+                    filteredProducts = filteredProducts.sort(
+                        (a, b) => b.price - a.price
+                    );
+                    break;
+                case "dateNewToOld":
+                    filteredProducts = filteredProducts.sort(
+                        (a, b) => new Date(b.date) - new Date(a.date)
+                    );
+                    break;
+                case "dateOldToNew":
+                    filteredProducts = filteredProducts.sort(
+                        (a, b) => new Date(a.date) - new Date(b.date)
+                    );
+                    break;
+                default:
+                    break;
+            }
 
-      setProducts(filteredProducts);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            setProducts(filteredProducts);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const handleBrandFilterChange = (brand) => {
-    setSelectedBrand(brand);
-  };
-  const handleTagsFilterChange = (tags) => {
-    setSelectedTags(tags);
-  };
+    const handleBrandFilterChange = (brand) => {
+        setSelectedBrand(brand);
+    };
+    const handleTagsFilterChange = (tags) => {
+        setSelectedTags(tags);
+    };
 
-  const handleButtonClick = (category) => {
-    setActiveCategory(category);
-    setSelectedBrand("All");
-    setOnSort("priceLowToHigh");
-    setSelectedTags("All");
-    setCurrentPage(1);
-  };
+    const handleButtonClick = (category) => {
+        setActiveCategory(category);
+        setSelectedBrand("All");
+        setOnSort("priceLowToHigh");
+        setSelectedTags("All");
+        setCurrentPage(1);
+    };
 
-  React.useEffect(() => {
-    fetchProducts();
-  }, [activeCategory, onSort, selectedBrand, selectedTags]);
+    React.useEffect(() => {
+        fetchProducts();
+    }, [activeCategory, onSort, selectedBrand, selectedTags]);
 
-  const activeButtonStyle =
-    "w-15 h-[30px] bg-primary text-white border-none rounded-sm";
-  const inactiveButtonStyle =
-    "w-15 h-[30px] bg-[#F2F0FD] text-primary border-none rounded-sm";
+    const activeButtonStyle =
+        "w-15 h-[30px] bg-primary text-white border-none rounded-sm";
+    const inactiveButtonStyle =
+        "w-15 h-[30px] bg-[#F2F0FD] text-primary border-none rounded-sm";
 
-  return (
-    <Provider store={store}>
-      <div className="flex flex-col ">
-        <Navbar />
-        <main className="flex justify-between mt-5  ">
-          <aside className="w-[416px]">
-            <Sorter onSort={onSort} setOnSort={setOnSort} />
-            <BrandsFilter
-              products={products}
-              onFilterChange={handleBrandFilterChange}
-              selectedBrand={selectedBrand}
-              setSelectedBrand={setSelectedBrand}
-            />
-            <TagsFilter
-              products={products}
-              onFilterChange={handleTagsFilterChange}
-              selectedTag={selectedTags}
-              setSelectedTags={setSelectedTags}
-            />
-          </aside>
-          <section className="w-[608px]">
-            <h4 className="text-black500 text-xl font-normal tracking-[0.016em]">
-              Products
-            </h4>
-            <div className="flex gap-2 mt-4">
-              <button
-                className={
-                  activeCategory === "mugs"
-                    ? activeButtonStyle
-                    : inactiveButtonStyle
-                }
-                onClick={() => handleButtonClick("mugs")}
-              >
-                mug
-              </button>
-              <button
-                className={
-                  activeCategory === "shirts"
-                    ? activeButtonStyle
-                    : inactiveButtonStyle
-                }
-                onClick={() => handleButtonClick("shirts")}
-              >
-                shirt
-              </button>
+    return (
+        <Provider store={store}>
+            <div className="flex flex-col ">
+                <Navbar />
+                <main className="flex justify-between mt-5  ">
+                    <aside className="w-[416px]">
+                        <Sorter onSort={onSort} setOnSort={setOnSort} />
+                        <BrandsFilter
+                            products={products}
+                            onFilterChange={handleBrandFilterChange}
+                            selectedBrand={selectedBrand}
+                            setSelectedBrand={setSelectedBrand}
+                        />
+                        <TagsFilter
+                            products={products}
+                            onFilterChange={handleTagsFilterChange}
+                            selectedTags={selectedTags}
+                            setSelectedTags={setSelectedTags}
+                        />
+                    </aside>
+                    <section className="w-[608px]">
+                        <h4 className="text-black500 text-xl font-normal tracking-[0.016em]">
+                            Products
+                        </h4>
+                        <div className="flex gap-2 mt-4">
+                            <button
+                                className={
+                                    activeCategory === "mugs"
+                                        ? activeButtonStyle
+                                        : inactiveButtonStyle
+                                }
+                                onClick={() => handleButtonClick("mugs")}>
+                                mug
+                            </button>
+                            <button
+                                className={
+                                    activeCategory === "shirts"
+                                        ? activeButtonStyle
+                                        : inactiveButtonStyle
+                                }
+                                onClick={() => handleButtonClick("shirts")}>
+                                shirt
+                            </button>
+                        </div>
+                        <div className="grid grid-cols-4 gap-6 my-5 border rounded-sm shadow-sm bg-white p-5">
+                            {displayedProducts.map((product) => (
+                                <ProductCard key={product.id} {...product} />
+                            ))}
+                        </div>
+                        <div className="w-full items-center flex justify-center mb-5">
+                            <Pagination
+                                totalProducts={products.length}
+                                productsPerPage={productsPerPage}
+                                currentPage={currentPage}
+                                onPageChange={handlePageChange}
+                            />
+                        </div>
+                    </section>
+                    <aside className="w-[416px] mt-10 ml-4 ">
+                        {" "}
+                        <BasketCard />
+                    </aside>
+                </main>
             </div>
-            <div className="grid grid-cols-4 gap-6 my-5 border rounded-sm shadow-sm bg-white p-5">
-              {displayedProducts.map((product) => (
-                <ProductCard key={product.id} {...product} />
-              ))}
-            </div>
-            <div className="w-full items-center flex justify-center mb-5">
-              <Pagination
-                totalProducts={products.length}
-                productsPerPage={productsPerPage}
-                currentPage={currentPage}
-                onPageChange={handlePageChange}
-              />
-            </div>
-          </section>
-          <aside className="w-[416px] mt-10 ml-4 ">
-            {" "}
-            <BasketCard />
-          </aside>
-        </main>
-      </div>
-    </Provider>
-  );
+        </Provider>
+    );
 };
 
 export default Page;
